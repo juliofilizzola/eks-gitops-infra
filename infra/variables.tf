209196a48364c0501d3b8e48b52869c736864535
julio filizzola <juliofilizzola@hotmail.com>
2026-01-31 09:20:40 -0300

    Add Terraform variables for cluster configuration (name, region, VPC CIDR)

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,3 @@
-
+variable "cluster_name" { default = "kibana-cluster" }
+variable "region" { default = "us-east-1" }  # Mude para sa-east-1 se preferir BR
+variable "vpc_cidr" { default = "10.0.0.0/16" }
